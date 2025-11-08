20ce84a867de47ecab4141822972f5238f47fda5
Peter Lahanas <plahanas18@gmail.com>
2025-11-08 13:41:33 +1000

    Updated log file location

--- htg/__init__.py
+++ htg/__init__.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
     handlers=[
-        RotatingFileHandler('./logs.log', maxBytes=5_000_000, backupCount=5),
+        RotatingFileHandler('./logs/htg.log', maxBytes=5_000_000, backupCount=5),
         logging.StreamHandler(),
     ],
 )
