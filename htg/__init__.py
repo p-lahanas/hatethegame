51b29c100c6aca73a63289da0f66373f36bb56f3
Peter Lahanas <plahanas18@gmail.com>
2025-11-08 20:03:23 +1000

    Write log in project root

--- htg/__init__.py
+++ htg/__init__.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
     handlers=[
-        RotatingFileHandler('./logs/htg.log', maxBytes=5_000_000, backupCount=5),
+        RotatingFileHandler('./htg.log', maxBytes=5_000_000, backupCount=5),
         logging.StreamHandler(),
     ],
 )
